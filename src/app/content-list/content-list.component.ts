b7f0cb4ac6d1df683491d6bad1fecbc73ebb2705
MalviKanadia <W0849019@myscc.ca>
2024-09-25 17:48:56 -0400

    step 9 completed

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {User} from "../Shared/models/user";
+import {ContentListItemComponent} from "../content-list-item/content-list-item.component";
 
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ContentListItemComponent
+  ],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.css'
 })
 export class ContentListComponent {
+  userList: User[] =[
+    {id:1, productName:"WaterBottle", quantity:1, color:"cyan", giftWrap:false,},
+    {id:2, productName:"Bag", quantity:1, color:"black",  giftWrap:false,},
+    {id:3, productName:"LunchBox",  quantity:2, color:"biege",  giftWrap:true},
+    {id:4, productName:"Box", quantity:4, color:"purple", giftWrap:true}]
 
 }
