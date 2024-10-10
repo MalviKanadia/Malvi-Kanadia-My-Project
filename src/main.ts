88141a4f976ac9ded0184e9d6db00a0dcbcf2f70
MalviKanadia <W0849019@myscc.ca>
2024-10-10 14:10:42 -0400

    step 3 and step4 and step5 completed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {ContentListComponent} from "./app/content-list/content-list.component";
+import {ContentListItemComponent} from "./app/content-list-item/content-list-item.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path:'',redirectTo: '/items', pathMatch: 'full'},
+  {path:'items',component:ContentListComponent},
+  {path:'items/:id',component:ContentListItemComponent},
+  {path: 'modify-item',component:ModifyListItemComponent},
+  {path: '**', component:PageNotFoundComponent},
+
+];
+bootstrapApplication(AppComponent,{
+  providers:[provideRouter(routes)]
+
+});
