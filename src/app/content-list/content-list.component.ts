29610f5d2ae06c9b714612d0a703d222a6d481eb
MalviKanadia <W0849019@myscc.ca>
2024-10-31 20:04:34 -0400

    step3, step4 and step5 completed of assignment6

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -3,7 +3,8 @@ import {User} from "../Shared/models/user";
 import {ContentListItemComponent} from "../content-list-item/content-list-item.component";
 import {NgClass, NgForOf} from "@angular/common";
 import {MyStoreService} from "../Services/my-store.service";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-content-list',
@@ -21,8 +22,9 @@ export class ContentListComponent implements OnInit {
   displayedColumns: string[]=['id','productName','quantity','color','giftWrap']
   userList: User[] =[]
 
-  constructor(private myStoreService: MyStoreService) {
-  }
+
+  constructor(private myStoreService: MyStoreService , private router: Router)
+  {}
   ngOnInit() {
     this.myStoreService.getMyStore().subscribe({
       next:(data: User[]) => this.userList = data,
@@ -33,5 +35,12 @@ export class ContentListComponent implements OnInit {
   selectedItem?:User;
   selectItem(item:User):void{
     this.selectedItem=item;
+  }
+  delete(id : number):void{
+    this.userList = this.userList.filter(item => item.id !== id);
+  }
+
+  navigateToItemList(): void {
+    this.router.navigate(['/modify-item']);
   }
 }
--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {User} from "../Shared/models/user";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MyStoreService} from "../Services/my-store.service";
+import {catchError, map, of, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-modify-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule,
+  ],
   templateUrl: './modify-list-item.component.html',
   styleUrl: './modify-list-item.component.css'
 })
-export class ModifyListItemComponent {
+export class ModifyListItemComponent implements OnInit {
+  itemForm: FormGroup;
+  items: User | undefined;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private myStoreService: MyStoreService,
+    private router: Router
+  ) {
+    this.itemForm = this.fb.group({
+      id: ['', Validators.required],
+      productName: ['', Validators.required],
+      quantity: ['', Validators.required],
+      color: [''],
+      giftWrap: [false]
+    });
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.myStoreService.getItemById(+id).subscribe(items => {
+        if(items) {
+          this.items = items;
+
+          this.itemForm.patchValue(items);
+        }
+      });
+    }
+  }
+
+  onSubmit(): void {
+    const items: User = this.itemForm.value;
+    if (items.id) {
+      this.myStoreService.updateItem(items);
+    } else {
+      const newId = this.myStoreService.generateNewId();
+      items.id = newId;
+      this.myStoreService.addItem(items);
+    }
+
+    this.router.navigate(['/items']);
+  }
+
+
+
+
 
 }
