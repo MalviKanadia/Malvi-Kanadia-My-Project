d9643941bf5b79d3cf57c09ac0f3f6020985730b
MalviKanadia <W0849019@myscc.ca>
2024-11-05 22:53:37 -0500

    step7 completed of assignment7

--- src/app/Services/my-store.service.ts
+++ src/app/Services/my-store.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {User} from "../Shared/models/user";
 import {userList} from "../Shared/data/mock-content";
-import {Observable, of} from "rxjs";
+import {catchError, Observable, of, throwError} from "rxjs";
+import {HttpClient,HttpErrorResponse} from "@angular/common/http";
 
 
 
@@ -9,38 +10,40 @@ import {Observable, of} from "rxjs";
   providedIn: 'root'
 })
 export class MyStoreService {
+  private apiUrl =  'api/items';
+
   private items: User[] = userList;
-  constructor() { }
+  constructor(private http: HttpClient) { }
     getMyStore(): Observable<User[]> {
-     return of (this.items);
+     return this.http.get<User[]>(this.apiUrl).pipe(catchError(this.handleError));
     }
     //getting item by id
-    getItemById(id: number): Observable<User | undefined> {
-      return of(this.items.find(item => item.id === id));
+    getItemById(id: number): Observable<User > {
+      return this.http.get<User>(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError));
     }
 
     //adding item
-    addItem(item: User) : Observable <User>{
-    this.items.push(item)
-    return of (item);
+    addItem(item: User) : Observable<User>{
+    item.id=this.generateNewId();
+    return this.http.post<User>(this.apiUrl, item).pipe(catchError(this.handleError));
     }
 
     //updating item
     updateItem(updatedItem: User): Observable <User | undefined>{
-    const index = this.items.findIndex(item => item.id === updatedItem.id);
-    if(index > -1){
-      this.items[index]=updatedItem;
-      return of(updatedItem);
-    }
-    return of (undefined);
+    const url= `${this.apiUrl}/${updatedItem.id}`;
+    return this.http.put<User>(url, updatedItem).pipe(catchError(this.handleError));
     }
 
     //deleting item
-    deleteItem(id: number) : Observable <User[]>{
-    this.items = this.items.filter(item => item.id !== id);
-    return of (this.items);
+    deleteItem(id: number) : Observable <{}>{
+    const url= `${this.apiUrl}/${id}`;
+    return this.http.delete(url).pipe(catchError(this.handleError));
     }
   generateNewId(): number {
     return this.items.length > 0 ? Math.max(...this.items.map(item => item.id)) + 1 : 1;
+  }
+  private handleError(error: HttpErrorResponse) {
+    console.error('API error', error);
+    return throwError(()=> new Error('server error, please try again.'));
   }
 }
